90b26f94ac385ef1b9a758f039d3928cf536ad39
John Kyle Cronan <john@pbx.org>
2022-03-04 15:40:39 -0600

    framework for app

---
+++ reviewpanel/apps.py
@@ -0,0 +1,18 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+from . import __version__
+
+
+class ReviewPanelConfig(AppConfig):
+    name = 'reviewpanel'
+    verbose_name = 'reviewpanel'
+    
+    class FormativePluginMeta:
+        name = 'reviewpanel'
+        author = 'John Kyle Cronan'
+        description = _('Review and score application form submissions')
+        version = __version__
+    
+    def ready(self):
+        pass
